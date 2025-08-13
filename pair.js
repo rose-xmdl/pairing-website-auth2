41972c8c37a9478fa7b1ed49a461eed01e353ef4
rose-xmdl <rosejades112@gmail.com>
2025-08-13 19:47:38 +0100

    Create pair.js

---
+++ pair.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore
+} = require("baileys");
+
+if (!fs.existsSync('./session')) {
+    fs.mkdirSync('./session', { recursive: true });
+}
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    let num = req.query.number;
+        async function Pair() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState(`./session`)
+     try {
+            let EliteProTech = makeWASocket({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: [ "Ubuntu", "Chrome", "20.0.04" ],
+             });
+             if(!EliteProTech.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await EliteProTech.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            EliteProTech.ev.on('creds.update', saveCreds)
+            EliteProTech.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(10000);
+                    const session = fs.readFileSync('./session/creds.json');
+      
+                    EliteProTech.groupAcceptInvite("Kjm8rnDFcpb04gQNSTbW2d");
+				const ses = await EliteProTech.sendMessage(EliteProTech.user.id, { document: session, mimetype: `application/json`, fileName: `creds.json` });
+				await EliteProTech.sendMessage(EliteProTech.user.id, { text: `✅ *SESSION ID OBTAINED SUCCESSFULLY!*  
+📁 Upload SESSION_ID (creds.json) on session folder or add it to your .env file: SESSION_ID=
+
+📢 *Stay Updated — Follow Our Channels:*
+
+➊ *WhatsApp Channel*  
+https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g
+
+➋ *Telegram*  
+https://t.me/elitepro_md
+
+➌ *YouTube*  
+https://youtube.com/@eliteprotechs
+
+🚫 *Do NOT share your session ID or creds.json with anyone.*
+
+🌐 *Explore more tools on our website:*  
+https://eliteprotech.zone.id` }, {quoted: ses});
+        await delay(100);
+        return await removeFile('./session');
+        process.exit(0)
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    Pair();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./session');
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await Pair()
+});
+
+process.on('uncaughtException', function (err) {
+let e = String(err)
+if (e.includes("conflict")) return
+if (e.includes("Socket connection timeout")) return
+if (e.includes("not-authorized")) return
+if (e.includes("rate-overlimit")) return
+if (e.includes("Connection Closed")) return
+if (e.includes("Timed Out")) return
+if (e.includes("Value not found")) return
+console.log('Caught exception: ', err)
+})
+
+module.exports = router
